30fc787b6daa0074fcb20e2ed569383cbf3e136b
JIJIN12 <jijinsuresh6@gmail.com>
2024-04-20 16:41:21 +0530

    Update shopcategoryslice.js

--- client/src/components/redux/slice/shopcategoryslice.js
+++ client/src/components/redux/slice/shopcategoryslice.js
@@ -3,39 +3,39 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-    Loading:false,
-    error:false,
-    allitem_data:[]
-}
+  Loading: false,
+  error: false,
+  allitem_data: [],
+};
 
-export const allitemdetails = createAsyncThunk('allitemdetails',async ()=>{
-    try {
-        console.log('start');
-        const response = await axios.get('https://industrial-tools-3.onrender.com/allitems')
+export const allitemdetails = createAsyncThunk("allitemdetails", async () => {
+  try {
+    console.log("start");
+    const response = await axios.get(
+      "https://industrial-tools-3.onrender.com/allitems"
+    );
     console.log(response);
-    return response
-    } catch (error) {
-        console.log(error);
-    }
-    
-})
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const allitemSlice = createSlice({
-    name:'allitem',
-    initialState,
-    extraReducers:(builder)=>{
-        builder.addCase(allitemdetails.pending,(state,action)=>{
-            state.Loading = true
-        })
-        builder.addCase(allitemdetails.fulfilled,(state,action)=>{
-            state.Loading = false
-            state.allitem_data  = action.payload.data.details
+  name: "allitem",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(allitemdetails.pending, (state, action) => {
+      state.Loading = true;
+    });
+    builder.addCase(allitemdetails.fulfilled, (state, action) => {
+      state.Loading = false;
+      state.allitem_data = action.payload.data.details;
+    });
+    builder.addCase(allitemdetails.rejected, (state, action) => {
+      state.error = true;
+    });
+  },
+});
 
-        })
-        builder.addCase(allitemdetails.rejected,(state,action)=>{
-            state.error = true
-        })
-    }
-})
-
-export default allitemSlice.reducer
+export default allitemSlice.reducer;
